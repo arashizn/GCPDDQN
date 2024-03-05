261c5d9200ba28e16811a09e2e3061e7e3ff7647
张娜 <1322623105@qq.com>
2024-03-05 13:14:12 +0000

    删除文件 t.py

--- t.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-import numpy as np
-# g = nx.barabasi_albert_graph(n=10, m=3)
-# a=np.array(list(g.nodes().keys()))
-# print(a)
-# b= np.arange(20)
-# print(b)
-# print(np.setdiff1d(b, a))
-g = nx.read_gpickle('test50b1.gpickle')
